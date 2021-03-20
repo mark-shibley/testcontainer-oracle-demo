14c4374d93fdc09aacf7f26873f060e67ff11e4b
Mark Shibley <24659381+mark-shibley@users.noreply.github.com>
2021-03-20 09:22:22 -0700

    Rename main class name

--- src/main/java/com/ms/oracle/TestContainerOracleApplication.java
+++ src/main/java/com/ms/oracle/TestContainerOracleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CascadeDemoApplication {
+public class TestContainerOracleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CascadeDemoApplication.class, args);
+        SpringApplication.run(TestContainerOracleApplication.class, args);
     }
 
 }
