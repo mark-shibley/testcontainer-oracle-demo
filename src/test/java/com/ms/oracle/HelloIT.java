48d6685d8b84ec4f40d43b0541588a40b98f91eb
Mark Shibley <mark.shibley@medimpact.com>
2021-06-21 09:22:36 -0700

    Remove ExtendWith from test class

--- src/test/java/com/ms/oracle/HelloIT.java
+++ src/test/java/com/ms/oracle/HelloIT.java
@@ -6,18 +6,15 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.OracleContainer;
 
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ContextConfiguration(
         initializers = {HelloIT.TestContainerInitializer.class})
